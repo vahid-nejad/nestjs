56c8a9a0d5f2589401b121d8727a652ce5a6123e
Vahid Nejad <vahid884@gmail.com>
2024-08-01 18:29:55 +0330

    local strategy

--- src/auth/guards/local-auth/local-auth.guard.ts
+++ src/auth/guards/local-auth/local-auth.guard.ts
@@ -1,6 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
